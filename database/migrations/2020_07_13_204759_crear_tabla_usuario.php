d1225ed5aa708d68b91d7b7667e412ea12307877
martinbrelis <martinbrelis@gmail.com>
2020-07-13 19:11:31 -0300

    cabios basicos commit

---
+++ database/migrations/2020_07_13_204759_crear_tabla_usuario.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre',40);
+            $table->string('password',100);
+            $table->string('observaciones',100);
+            $table->unsignedInteger('rol_id');
+            $table->foreign('rol_id','fk_usuario_rol')->references('id')->on('rol')->onDelete('restrict')->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
